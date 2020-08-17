b196e37396aae205d1e45a3567a84019d1b75203
Shashank Rai <rai.shashank@gmail.com>
2020-08-17 18:50:28 +0100

    Dont trigger snapshot backup if another snapshot is already in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,6 +14,10 @@ def filter_instances(project):
         instances = ec2.instances.all()
 
     return instances
+
+def has_pending_snapshots(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == "pending"
 @click.group()
 def cli():
     """Commands for Snapshots"""
@@ -81,6 +85,10 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshots(v):
+                print(" Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+            
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="created by snapshot-analyzer-301")
 
